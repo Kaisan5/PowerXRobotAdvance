b8df450b8072b3dc9635dc296ab177d03f9ce1bc
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-05-04 22:17:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,5 +103,11 @@ class Bot(Client):
 
 
 app = Bot()
-app.run()
 #..........................................
+from bot import Bot
+import pyrogram.utils
+
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+
+if __name__ == "__bot__":
+    Bot().run()
